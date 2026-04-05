bc84b938142d77c18eddab37acbaced8acd41ef5
gowthamjyothi <gb3453113@gmail.com>
2026-04-05 10:46:26 +0530

    Add files via upload

---
+++ python_basics_gow.py
@@ -0,0 +1,10 @@
+name="gowtham"
+age=18
+height=6.0
+student='true'
+print("name:",name,"age:",age,"height:",height,"student:",student)
+print(f"Name: {name} age: {age} height: {height} student: {student}")
+print(f"age in months:{age*12}")
+print(f"age in days:{age*365}")
+print(age%7)
+print(age**2)
